d139744074e9afbf2e950d1f7fa89771c57c5d18
WojtekWatroba <31944@student.pwsztar.edu.pl>
2019-10-25 08:10:12 +0200

    javas

---
+++ navbar.js
@@ -0,0 +1,3 @@
+let nav = '<ul><ul class="nav"><li class="nav-item"><a class="nav-link active" href="index.html">Home</a></li><li class="nav-item"><a class="nav-link" href="#">Link</a></li><li class="nav-item"><a class="nav-link" href="contact.html">kontakt</a></li></ul></ul>'
+let elem =document.querySelector('.navbar')
+elem.innerHTML = nav;
