613830c6196264260e06369490880a66b515294f
Dawid <evilmrdevill@gmail.com>
2019-11-08 09:34:01 +0100

    quiz

--- js/navbar.js
+++ js/navbar.js
@@ -1,3 +1,4 @@
-let nav = '<ul><ul class="nav"><li class="nav-item"><a class="nav-link active" href="index.html">Home</a></li><li class="nav-item"><a class="nav-link" href="#">Link</a></li><li class="nav-item"><a class="nav-link" href="contact.html">kontakt</a></li></ul></ul>'
+let nav = '<ul><ul class="nav"><li class="nav-item"><a class="nav-link active" href="index.html">Home</a></li><li class="nav-item"><a class="nav-link" href="#">Link</a></li><li class="nav-item"><a class="nav-link" href="contact.html">kontakt</a></li> <li class="nav-item"> <a class="nav-link" href="quiz.html">quiz</a></li></ul></ul>'
 let elem =document.querySelector('.navbar')
 elem.innerHTML = nav;
+
